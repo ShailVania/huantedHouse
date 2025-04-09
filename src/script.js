aa4e04a5752c8d8e5f401a32f0ea7e807c5e3506
Shail Vania <vanias@uwindsor.ca>
2025-04-09 17:47:24 +0530

    Added fog to the scene

--- src/script.js
+++ src/script.js
@@ -379,6 +379,9 @@ sky.material.uniforms['mieCoefficient'].value = 0.1
 sky.material.uniforms['mieDirectionalG'].value = 0.95
 sky.material.uniforms['sunPosition'].value.set(0.3, -0.038, -0.95)
 
+//Fog
+scene.fog = new THREE.FogExp2('#02343f', 0.1)
+
 /**
  * Animate
  */
